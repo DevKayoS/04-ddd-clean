36f3aebe278ed6eea5a1066171729e7231831dac
Kayo Vinicius <kayoribeirodev@gmail.com>
2024-08-12 12:21:32 -0300

    aprendendo sobre value objects

---
+++ src/domain/entities/value-objects.ts/slug.ts
@@ -0,0 +1,28 @@
+export class Slug {
+ public value: string
+
+  constructor(value: string){
+    this.value = value
+ } 
+ /**
+  *  Receives a tring and normalize it as  a slug.
+  * 
+  * Example: "An example title" => "an-exanmple-title"
+  * 
+  * @param text {string}
+  */
+
+ static createFromText(text: string){
+    const slugText = text
+    .normalize("NFKD")
+    .toLocaleLowerCase()
+    .trim() // retirar espaçamento da direita ou esquerda
+    .replace(/\s+/g, '-')
+    .replace(/[^\w-]+/g, '')
+    .replace(/_/g,'-')
+    .replace(/--+/g,'-')
+    .replace(/-$/g,'')
+
+    return new Slug(slugText)
+ }
+}
