289989cddc6eb94f59caa1676150f90defece9ce
Kayo Vinicius <kayoribeirodev@gmail.com>
2024-08-11 23:50:01 -0300

    aprendendo mais sobre arquitetura e desing de software

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,11 +1,21 @@
 import { randomUUID } from "node:crypto"
 
+interface AnswerProps {
+  content: string
+  authorId: string
+  questionId: string
+}
+
 export class Answer {
   public id: string
   public content: string
+  public authorId: string
+  public questionId: string
 
-  constructor(content: string,  id?: string){
+  constructor({authorId, content, questionId}:AnswerProps, id?: string){
     this.id = id ??  randomUUID()
+    this.authorId = authorId
+    this.questionId = questionId
     this.content = content
   }
 }
