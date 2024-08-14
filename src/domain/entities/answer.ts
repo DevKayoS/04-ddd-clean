e0d8050a1d51a483c7c145f1f31bf6cbba7626c9
Kayo Vinicius <kayoribeirodev@gmail.com>
2024-08-14 10:18:07 -0300

    aprendendo a lidar com getters e setters

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,6 +1,7 @@
 import { Entity } from "../../core/entities/entity"
 import { UniqueEntityId } from "../../core/entities/unique-entity-id"
 import { Optional } from "../../core/types/optional"
+import { Question } from "./question"
 
 interface AnswerProps {
   authorId: UniqueEntityId
@@ -13,6 +14,40 @@ interface AnswerProps {
 export class Answer extends Entity<AnswerProps>{
   get content(){
     return this.props.content
+  }
+  get authorId(){
+    return this.props.authorId
+  }
+  get questionId(){
+    return this.props.questionId
+  }
+  get createdAt(){
+    return this.props.createdAt
+  }
+  get updatedAt(){
+    return this.props.updatedAt
+  }
+
+  get excerpt() {
+    /**
+     * basicamente fazendo um resumo da resposta ele vai pegar o conteudo e pegar 
+     * os primeiros 120 caracteres e no final vai substituir o espaço vazio por ...
+     * 
+     * ex:  
+     * da pra vc fazer tal coisa no arch linux fazendo bla bla bla bla bla bla bla bla bla e com isso vc pega o hyprland e...
+     *
+     */
+    return this.content.substring(0, 120).trimEnd().concat('...')
+  }
+  
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
+  set content(content: string) {
+    this.props.content = content
+    // atualizando o conteudo
+    this.touch()
   }
 
   static create(props: Optional<AnswerProps, 'createdAt'>, id?: UniqueEntityId){
--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -3,6 +3,7 @@ import { Slug } from "./value-objects.ts/slug"
 import { Entity } from "../../core/entities/entity"
 import { UniqueEntityId } from "../../core/entities/unique-entity-id"
 import { Optional } from "../../core/types/optional"
+import dayjs from "dayjs"
 
 interface QuestionProps {
   authorId: UniqueEntityId
@@ -15,9 +16,67 @@ interface QuestionProps {
 }
 
 export class Question extends Entity<QuestionProps>{
-  static create(props: Optional<QuestionProps, 'createdAt'>, id?: UniqueEntityId){
+  get authorId(){
+    return this.props.authorId
+  }
+  get bestAnswerId(){
+    return this.props.bestAnswerId
+  }
+  get title(){
+    return this.props.title
+  }
+  get content(){
+    return this.props.content
+  }
+   get slug(){
+    return this.props.slug
+  }
+  get createdAt(){
+    return this.props.createdAt
+  }
+  get updatedAt(){
+    return this.props.updatedAt
+  }
+
+  get isNew(): boolean {  
+    /** 
+     * pega a funcao do dayjs e faz a diferenca em DIAS da data atual para a data
+     * de criacao do objeto e se for menor que 3 ela e ua pergunta recente
+     * 
+     * se for uma diferenca menor do que 3 dias ira retornar true 
+     * se for uma diferenca maior do que 3 dias ira retornar false  
+    */
+    return dayjs().diff(this.createdAt, 'days') <= 3
+  }
+
+  get excerpt() {
+    return this.content.substring(0, 120).trimEnd().concat('...')
+  }
+  
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+  
+  set title(title: string) {
+    this.props.title = title
+    this.props.slug = Slug.createFromText(title)
+    // atualizando o conteudo
+    this.touch()
+  }
+  set content(content: string) {
+    this.props.content = content
+    this.touch()
+  }
+
+  set bestAnswerId(bestAnswerId: UniqueEntityId | undefined) {
+    this.props.bestAnswerId = bestAnswerId
+    this.touch()
+  }
+
+  static create(props: Optional<QuestionProps, 'createdAt' | 'slug'>, id?: UniqueEntityId){
     const question = new Question({
       ...props,
+      slug: props.slug ?? Slug.createFromText(props.title),
       createdAt: new Date()
     }, id)
 
