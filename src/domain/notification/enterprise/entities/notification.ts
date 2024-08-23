c7553aada52fe35c97b7b9c85ed745bc2a5dad25
Kayo Vinicius <kayoribeirodev@gmail.com>
2024-08-23 13:47:27 -0300

    adicioanado caso de uso para ler notificação

--- src/domain/notification/enterprise/entities/notification.ts
+++ src/domain/notification/enterprise/entities/notification.ts
@@ -2,7 +2,7 @@ import { Entity } from "@/core/entities/entity";
 import { UniqueEntityId } from "@/core/entities/unique-entity-id";
 import { Optional } from "@/core/types/optional";
 
-interface NotificationProps {
+export interface NotificationProps {
   recipientId: UniqueEntityId
   title: string
   content: string
@@ -29,10 +29,16 @@ export class Notification extends Entity<NotificationProps> {
     return this.props.createdAt
   }
 
+  read(){
+    this.props.readAt = new Date()
+  }
+
   static create(props: Optional<NotificationProps, 'createdAt'>, id?: UniqueEntityId) {
     const notification = new Notification({
       ...props,
       createdAt: props.createdAt ?? new Date() 
-    })
+    }, id)
+
+    return notification
   }
 }
