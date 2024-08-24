4752673fabaf7468b32325e426f9e3f5a5eee9ec
Kayo Vinicius <kayoribeirodev@gmail.com>
2024-08-24 15:30:29 -0300

    adicionando o envio da notificacao e criado os testes

--- src/domain/notification/application/subscriber/on-answer-created.spec.ts
+++ src/domain/notification/application/subscriber/on-answer-created.spec.ts
@@ -2,22 +2,48 @@ import { makeAnswer } from "test/factories/make-answer"
 import { OnAnswerCreated } from "./on-answer-created"
 import { InMemoryAnswerRepository } from "test/repositories/in-memory-answer-repository"
 import { InMemoryAnswerAttachmentsRepository } from "test/repositories/in-memory-answer-attachment"
+import { SendNotificationUseCase, SendNotificationUseCaseRequest, SendNotificationUseCaseResponse } from "../use-cases/send-notification"
+import { InMemoryQuestionRepository } from "test/repositories/in-memory-question-repository"
+import { InMemoryQuestionAttachmentsRepository } from "test/repositories/in-memory-question-attachment-repository"
+import { InMemoryNotificationsRepository } from "test/repositories/in-memory-notification-repository"
+import { makeQuestion } from "test/factories/make-question"
+import { waitFor } from "test/utils/wait-for"
 
+let inMemoryQuestionsRepository: InMemoryQuestionRepository
+let inMemoryQuestionAttachmentRepository: InMemoryQuestionAttachmentsRepository
+let sendNotificationUseCase: SendNotificationUseCase
 let inMemoryAnswerRepository: InMemoryAnswerRepository
 let inMemoryAnswerAttachmentRepository: InMemoryAnswerAttachmentsRepository
+let inMemoryNotificationsRepository: InMemoryNotificationsRepository
+
+let sendNotificationExecuteSpy: SpyInstance<
+  [SendNotificationUseCaseRequest],
+  Promise<SendNotificationUseCaseResponse>
+>;
 
 describe('On Answer Created', ()=> {
   beforeEach(()=> {
+    inMemoryNotificationsRepository = new InMemoryNotificationsRepository()
+    sendNotificationUseCase = new SendNotificationUseCase(inMemoryNotificationsRepository)
+    inMemoryQuestionAttachmentRepository = new InMemoryQuestionAttachmentsRepository()
+    inMemoryQuestionsRepository = new InMemoryQuestionRepository(inMemoryQuestionAttachmentRepository)
     inMemoryAnswerAttachmentRepository = new InMemoryAnswerAttachmentsRepository()
     inMemoryAnswerRepository = new InMemoryAnswerRepository(inMemoryAnswerAttachmentRepository)
+
+    sendNotificationExecuteSpy = vi.spyOn(sendNotificationUseCase, 'execute')
+
+    new OnAnswerCreated(inMemoryQuestionsRepository, sendNotificationUseCase)
   })
 
   it('should send a notification when an answer ios created', async()=> {
-    const onAnswerCreated = new OnAnswerCreated()
+    const question = makeQuestion()
+    const answer = makeAnswer({questionId: question.id})
 
-    const answer = makeAnswer()
-
+    inMemoryQuestionsRepository.create(question)
     inMemoryAnswerRepository.create(answer)
 
+    await waitFor(()=> {
+      expect(sendNotificationExecuteSpy).toHaveBeenCalled()
+    } )
   })
 })
--- src/domain/notification/application/subscriber/on-answer-created.ts
+++ src/domain/notification/application/subscriber/on-answer-created.ts
@@ -1,9 +1,14 @@
 import { DomainEvents } from "@/core/events/domain-events";
 import { EventHandler } from "@/core/events/event-handler";
+import { QuestionRepository } from "@/domain/forum/application/repositories/question-repository";
 import { AnswerCreatedEvent } from "@/domain/forum/enterprise/events/answer-created-event";
+import { SendNotificationUseCase } from "../use-cases/send-notification";
 
 export class OnAnswerCreated implements EventHandler {
-  constructor() {
+  constructor(
+    private questionsRepository: QuestionRepository,
+    private sendNotificationUseCase: SendNotificationUseCase
+  ) {
     this.setupSubscriptions()
   }
 
@@ -15,6 +20,15 @@ export class OnAnswerCreated implements EventHandler {
   }
 
   private async sendNewAnswerNotification({ answer }: AnswerCreatedEvent) {
-    console.log(answer)
+    const question = await this.questionsRepository.findById(answer.questionId.toString())
+
+    if(question) {
+      await this.sendNotificationUseCase.execute({
+        recipientId: question.authorId.toString(),
+        title: `Nova reposta em "${question.title.substring(0, 40).concat('...')}"`,
+        content: answer.excerpt,
+      })
+    }
+
   }
 }
